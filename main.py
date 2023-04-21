dfa9839349969e52027765a21e54153de7aeda05
Caseymonroe1 <tun04492@temple.edu>
2023-04-20 21:34:54 -0400

    Add files via upload
    Added announcement feature, getting strange bug where it works selectively with some courses.  It times out for some courses with announcements and all courses without announcements.  Not sure if my code is wrong or there is a bug with the canvas api

---
+++ main.py
@@ -0,0 +1,122 @@
+import discord
+from bs4 import BeautifulSoup
+from discord.ext import commands
+
+import canvasapi
+import datetime 
+import pytz
+
+DISCORD = 'MTA3NTA3MDQ4NTQzNzYyODQ5Ng.GUNLqu.FAph9PM6AXXUsRPLynBJn-MMJn5k7X2DUWWn2k'
+CANVAS = '9957~z5ERya9W6uFKzAfoB31fgHj0wMlyGkywwngUjbpc4HsVaeNHDXsThPVUljTc8q9U'
+BASEURL = 'https://templeu.instructure.com/'
+canvas_api = canvasapi.Canvas(BASEURL, CANVAS)
+
+current_class = canvas_api.get_courses()[5]
+
+bot = commands.Bot(command_prefix = "&", intents = discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Canvas Info Bot up and running!")
+
+@bot.command()
+async def course(ctx):
+    await ctx.send("Here are your courses:\n")
+
+    courses = canvas_api.get_courses(enrollment_state='active')
+
+    select = 0
+    for course in courses:
+        name = course.name
+        id = course.id
+        await ctx.send(f"({select}) {name}\n")
+        select += 1
+
+    await ctx.send(f"Enter a number to select the corresponding course\n") 
+
+    def check(m):
+        if m.content.isdigit():
+            global pick 
+            pick = int(m.content)
+            return range(0,select).count(pick) > 0
+
+    msg = await bot.wait_for('message', check=check, timeout = 15)
+    print(courses[pick].id)
+    global current_class 
+    current_class = canvas_api.get_course(courses[pick].id)
+    await ctx.send(f'Current course: **{courses[pick].name}**\n')
+
+@bot.command()
+async def announcements(ctx):
+    user = canvas_api.get_user('self')
+    courses=user.get_courses()
+    courselist=[]
+    await ctx.send("Pick to see announcements from a course:\n")
+
+    courses = canvas_api.get_courses(enrollment_state='active')
+
+    select = 0
+    for course in courses:
+        name = course.name
+        id = course.id
+        await ctx.send(f"({select}) {name}\n")
+        select += 1
+
+    await ctx.send(f"Enter a number to select the corresponding course\n") 
+
+    def check(m):
+        if m.content.isdigit():
+            global pick 
+            pick = int(m.content)
+            return range(0,select).count(pick) > 0
+
+    msg = await bot.wait_for('message', check=check, timeout = 15)
+    print(courses[pick].id)
+    global current_class 
+    current_class = canvas_api.get_course(courses[pick].id)
+    await ctx.send("Here are your announcements for this course\n")
+    test  = canvas_api.get_announcements(context_codes=[courses[pick].id])
+    if(test is None):
+        await ctx.send("No announcements for this course")
+        return
+    for a in test:
+        html=a.message
+        soup = BeautifulSoup(html, features="html.parser")
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
+        text = soup.get_text()
+        print(text)
+        posted_at = datetime.datetime.strptime(a.posted_at, '%Y-%m-%dT%H:%M:%SZ')
+        formatted_date = posted_at.strftime('%B %d, %Y at %I:%M %p')
+        await ctx.send(formatted_date)
+        await ctx.send(text)
+
+
+@bot.command()
+async def upcoming(ctx):
+    none_upcoming = True
+
+    user = canvas_api.get_user('self')
+
+    print(user.name)
+
+    #softwareDesign = canvas_api.get_course(123654) 
+    assignments = current_class.get_assignments()
+
+    for assignment in assignments:
+        due_date = str(assignment.due_at)
+
+        if(due_date != "None"):
+            t1 = datetime.datetime(int(due_date[0:4]), int(due_date[5:7]), int(due_date[8:10]))
+            t2 = datetime.datetime.now()
+            if(t1>t2):
+                none_upcoming = False
+                readable_time = f"{due_date[11:16]} UTC"
+                readable_date = t1.strftime("%A, %B %d")
+                print(f"{assignment} is due on {readable_date} at {readable_time}\n")
+                await ctx.send(f"```diff\n- {assignment.name} -\ndue on {readable_date} at {readable_time}```\n\n")
+    
+    if(none_upcoming):
+        await ctx.send(f"You have no upcoming assignments in {current_class.name}!")
+
+bot.run(DISCORD)
